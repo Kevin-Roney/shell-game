baad95c9cb1d6231b0a3cf2fa8345a99457afc49
Kevin Roney <ktroney.pdx@gmail.com>
2022-03-08 11:28:43 -0800

    added shells of eventlisteners

--- app.js
+++ app.js
@@ -7,12 +7,23 @@ const button1 = document.getElementById('cup1Button');
 const button2 = document.getElementById('cup2Button');
 const button3 = document.getElementById('cup3Button');
 
+const correctEl = document.getElementById('correct-guesses');
+const incorrectEl = document.getElementById('incorrect-guesses');
+const totalEl = document.getElementById('total-guesses');
+
+
 // let state
 let totalGuess = 0;
 let correctGuess = 0;
 let incorrectGuess = 0;
 // set event listeners 
 button1.addEventListener('click', () => {
+    const correctCup = getRandomCup();
+});
+button2.addEventListener('click', () => {
+
+});
+button3.addEventListener('click', () => {
 
 });
   // get user input
