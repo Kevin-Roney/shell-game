beb9163607041e5c3c0467b974770c88610b31d5
Kevin Roney <ktroney.pdx@gmail.com>
2022-03-08 11:20:53 -0800

    imported DOM elements

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 // import functions and grab DOM elements
+const cup1El = document.getElementById('cup1');
+const cup2El = document.getElementById('cup2');
+const cup3El = document.getElementById('cup3');
+
+const button1 = document.getElementById('cup1Button');
+const button2 = document.getElementById('cup2Button');
+const button3 = document.getElementById('cup3Button');
 
 // let state
-
+let totalGuess = 0;
+let correctGuess = 0;
+let incorrectGuess = 0;
 // set event listeners 
+button1.addEventListener('click', () => {
+
+});
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
